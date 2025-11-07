397aecff87ab11dde8951b974de56ae05d113c6d
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2025-11-07 11:43:26 +0100

    [Model]: todo model

---
+++ src/app/todos/models/todo.dto.ts
@@ -0,0 +1,12 @@
+export class TodoDTO{
+    id: number;
+    title: string;
+    done: boolean;
+
+    constructor(title: string){
+        this.id = new Date().getTime();
+        this.title = title;
+        this.done = false;
+    }
+
+}   
