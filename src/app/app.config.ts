50f7d533c887fd26ddc8ea9ccc3ce719a36d6105
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2025-11-07 12:13:24 +0100

    [Config]: reduz dev tools config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,23 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { TodoReducer } from './todos/todo.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideStore({
+      todos: TodoReducer // indicamos como se gestiona la store, indicando el estado y el gestor de las acciones (reducer)
+    }),
+    provideStoreDevtools({
+      maxAge: 25, // preserva un máximo de 25 estados en el historial
+      logOnly: environment.production, //Deshabilita las características de "tiempo-viaje" en producción
+      autoPause: true, //Pausa la grabación cuandop la pestaña DevTools está inactiva
+    })
   ]
 };
