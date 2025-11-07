157e74126286487f68188ccb94bad8c17803ba79
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2025-11-07 11:42:56 +0100

    [Structure]: new components

---
+++ src/app/todos/todo-list-item/todo-list-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-item',
+  imports: [],
+  templateUrl: './todo-list-item.html',
+  styleUrl: './todo-list-item.scss',
+})
+export class TodoListItem {
+//MUESTRA DETALLES DE LA TAREA: nombre, acciones: editar, cpmpletar, eliminar
+}
