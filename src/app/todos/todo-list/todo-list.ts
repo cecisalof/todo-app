e6809e401355804da9aada5706c03ae9e4d93300
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2025-11-10 12:54:50 +0100

    [Feature]: use selectTodos in mother component

--- src/app/todos/todo-list/todo-list.ts
+++ src/app/todos/todo-list/todo-list.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoListItem } from "../todo-list-item/todo-list-item";
+import { TodoDTO } from '../models/todo.dto';
+import { AppState } from '../../app.reducer';
+import { Store } from '@ngrx/store';
+import { selectTodos } from '../todo.selector';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [],
+  imports: [TodoListItem, CommonModule],
   templateUrl: './todo-list.html',
   styleUrl: './todo-list.scss',
 })
-export class TodoList {
-//LISTA DE TAREAS
+
+export class TodoList implements OnInit {
+ // LISTA DE TAREAS
+ // El Observable vive en el componente padre
+ todos$!: Observable<TodoDTO[]>
+
+ constructor(private store: Store<AppState>){ }
+  
+ ngOnInit(): void {
+  //Obtengo todos$
+   this.todos$= this.store.select(selectTodos)
+ }
+
 }
