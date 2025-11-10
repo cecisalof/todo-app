af078571f7b1a5d10de0cf261d6f4aed2af073c6
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2025-11-10 12:55:37 +0100

    [Feature]: Declare todo variable as an Input

--- src/app/todos/todo-list-item/todo-list-item.ts
+++ src/app/todos/todo-list-item/todo-list-item.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectTodos } from '../todo.selector';
+import { Observable } from 'rxjs';
+import { TodoDTO } from '../models/todo.dto';
+import { AppState } from '../../app.reducer';
 
 @Component({
   selector: 'app-todo-list-item',
@@ -7,5 +12,15 @@ import { Component } from '@angular/core';
   styleUrl: './todo-list-item.scss',
 })
 export class TodoListItem {
-//MUESTRA DETALLES DE LA TAREA: nombre, acciones: editar, cpmpletar, eliminar
+  // MUESTRA DETALLES DE LA TAREA: nombre, acciones: editar, cpmpletar, eliminar
+  // Declarar la variable todo dentro de la clase TodoListItem como una porpieda de entrada. Al añadir el Input le digo a Angular que todo recibirá datos de su componente padre.
+  // Solo recibe una tarea, no el array de tareas.
+  @Input() todo!: TodoDTO;
+
+  // constructor(private store: Store<AppState>) { }
+
+  ngOnInit(): void {
+    console.log(this.todo);
+  }
+
 }
